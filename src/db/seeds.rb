c7d1e28f9e0b4738dd3997cb081db8868781c456
YukiSato <yukiyukiamam@gmail.com>
2019-11-06 20:43:07 +0900

    [add]テストデータの追加
    seed.rb にテストデータを追加しました

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(image: '', name: '佐藤裕紀', age: 21, profession: '学生', content: '自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。')
+
+
+(1..10).each do |num|
+  Work.create!(image: '', title: "work_title_#{num}", content: "work_content_#{num}", url: 'http://herokuavv.dl', status: 1, release_at: Time.zone.now)
+end
+
+(1..10).each do |num|
+  History.create!(image: '', title: "history_title_#{num}", content: "history_content_#{num}", start_at: Time.zone.now.ago(3.month), end_at: Time.zone.now.ago(2.month))
+end
+
+(1..10).each do |num|
+  Skill.create!(name: "skill_name_#{num}")
+end
+
+(1..10).each do |num|
+  WorkSkill.create!(work_id: num, skill_id: num)
+end
