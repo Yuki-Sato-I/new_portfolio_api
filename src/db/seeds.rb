4d1946acaff3732b9f0d1e61675a595f1ef04feb
YukiSato <yukiyukiamam@gmail.com>
2019-11-10 15:55:18 +0900

    [change]seedファイルの変更
    seedファイルを変更しました

--- src/db/seeds.rb
+++ src/db/seeds.rb
@@ -5,15 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create!(image: '', name: '佐藤裕紀', age: 21, profession: '学生', content: '自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。')
+User.create!(image: "image", name: '佐藤裕紀', age: 21, profession: '学生', content: '自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。自己紹介が入ります。')
 
 
 (1..10).each do |num|
-  Work.create!(image: '', title: "work_title_#{num}", content: "work_content_#{num}", url: 'http://herokuavv.dl', status: 1, release_at: Time.zone.now)
+  Work.create!(image: "image_#{num}", title: "work_title_#{num}", content: "work_content_#{num}", url: "http://herokuavv.dl#{num}", status: 1, release_at: Time.zone.now.ago(num.month))
 end
 
 (1..10).each do |num|
-  History.create!(image: '', title: "history_title_#{num}", content: "history_content_#{num}", start_at: Time.zone.now.ago(3.month), end_at: Time.zone.now.ago(2.month))
+  History.create!(image: "image_#{num}", title: "history_title_#{num}", content: "history_content_#{num}", start_at: Time.zone.now.ago((num+2).month), end_at: Time.zone.now.ago(num.month))
 end
 
 (1..10).each do |num|
